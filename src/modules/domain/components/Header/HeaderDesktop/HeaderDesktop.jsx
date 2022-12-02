62e687571561289f1659d48c5a90f82ae6a44a16
l9sik <hotspot.by@gmail.com>
2022-12-03 02:48:49 +0300

    feat: mobile header v1

--- src/modules/domain/components/Header/HeaderDesktop/HeaderDesktop.jsx
+++ src/modules/domain/components/Header/HeaderDesktop/HeaderDesktop.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import s from './HeaderDesktop.module.scss';
 
 const HeaderDesktop = () => {
@@ -6,8 +7,24 @@ const HeaderDesktop = () => {
         <>
             <div className={s.Header}>
                 <div className={s.Container}>
-                    <div>menu</div>
-                    <div>signIN</div>
+                    <div className={s.Container_beginBox}><a>HEADER</a></div>
+                    <div className={s.Container_centerBox}>
+                        <ul className={s.List}>
+                            <li><a href='#'>Button 1</a></li>
+                            <li><a href='#'>Button 2</a></li>
+                            <li><a href='#'>Button 3</a></li>
+                        </ul>
+                    </div>
+                    <div className={
+                        classNames(
+                            s.Container_endBox,
+                            s.Container
+                        )
+                    }>
+                        <div><a href='#'>+375 (00) 000 00 00</a></div>
+                        <div className={s.Container_endBox_divider}>|</div>
+                        <div className={s.SwitchButton}><a href='#'>but</a></div>
+                    </div>
                 </div>
             </div>
         </>
