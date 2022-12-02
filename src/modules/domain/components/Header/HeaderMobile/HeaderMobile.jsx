9fee108b1a4a4d10f0173755d5de7f8f8628f82f
l9sik <hotspot.by@gmail.com>
2022-12-03 02:48:03 +0300

    feat: desktop header v1

--- src/modules/domain/components/Header/HeaderMobile/HeaderMobile.jsx
+++ src/modules/domain/components/Header/HeaderMobile/HeaderMobile.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import './HeaderMobile.module.scss';
+//import classNames from 'classnames';
+import s from './HeaderMobile.module.scss';
 
 const HeaderMobile = () => {
     return(
         <>
-            <div>Header mobile</div>
+            <div className={s.Container}>
+                <div className={s.Container_beginBox}><a href='#'>HEADER</a></div>
+                <div className={s.Container_endBox}>
+                    <div className={s.SwitchButton}><a href='#'>but</a></div>
+                    <div><a href='#'>HAM</a></div>
+                </div>
+            </div>
         </>
     );
 };
