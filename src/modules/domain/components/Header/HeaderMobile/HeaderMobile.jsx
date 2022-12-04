cc9aeb915729e27b6f91dab159a06e31d7d4a8d5
l9sik <hotspot.by@gmail.com>
2022-12-05 02:41:49 +0300

    feat: burger menu

--- src/modules/domain/components/Header/HeaderMobile/HeaderMobile.jsx
+++ src/modules/domain/components/Header/HeaderMobile/HeaderMobile.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 //import classNames from 'classnames';
+import { Nav, Navbar } from 'react-bootstrap';
+//import '../../../../../styles/font-sizes';
+//import c from '../../../../../styles/_colors.scss';
+//import HeaderMobileMenu from './HeaderMobileMenu';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import s from './HeaderMobile.module.scss';
-import HeaderMobileMenu from './HeaderMobileMenu';
 
 const HeaderMobile = () => {
     return(
         <>
-            <div className={s.Container} id='outer-container'>
-                <HeaderMobileMenu pageWrapId={'pageWrap'} outerContainerId={'outer-container'} />
-                <div className={s.Container_beginBox}><a href='#'>HEADER</a></div>
-                <div className={s.Container_endBox}>
-                    <div className={s.SwitchButton}><a href='#'>but</a></div>
-                    <div><a href='#'>HAM</a></div>
-                </div>
-            </div>
+            <Navbar collapseOnSelect expand="lg" variant='white' className={s.Container}>
+                <Navbar.Brand href="#home">
+                    <div className={s.Container_beginBox}><a href='#'>HEADER</a></div>
+                </Navbar.Brand>
+                <Navbar.Toggle className={s.Toggle} aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link className={s.List} href="#features">Button1</Nav.Link>
+                        <Nav.Link className={s.List} href="#pricing">Button2</Nav.Link>
+                        <Nav.Link className={s.List} href="#deets">Button3</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </>
     );
 };
