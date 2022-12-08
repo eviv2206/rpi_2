ea7a8520979eb6f9774d70cc6966827b31f82547
l9sik <hotspot.by@gmail.com>
2022-12-08 20:55:55 +0300

    feat: language switch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.module.scss';
 import { BrowserRouter } from 'react-router-dom';
+
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import './index.module.scss';
 import App from './common/components/App';
+import { Suspense } from 'react';
+i18next
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        supportedLngs: ['ru', 'en'],
+        fallbackLng: 'en',
+        //lng: document.querySelector('html').lang,
+        // Options for language detector
+        detection: {
+            order: ['cookie', 'htmlTag', 'localStorage', 'path'],
+            caches: ['cookie'],
+        },
+        // react: { useSuspense: false },
+        backend: {
+            //loadPath: 'D:/rpi3/rpi_2/src/assets/locales/{{lng}}/translation.json',
+            loadPath: 'src/assets/locales/{{lng}}/translation.json',
+            //'src/assets/locales/{{lng}}}/translation.json'
+        },
+    });
+
+const loadingMarkup = (
+    <div className="py-4 text-center">
+        <h3>Loading..</h3>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+    <Suspense fallback={loadingMarkup}>
+        <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </React.StrictMode>
+    </Suspense>
 );
--- src/modules/domain/components/Header/HeaderDesktop/HeaderDesktop.jsx
+++ src/modules/domain/components/Header/HeaderDesktop/HeaderDesktop.jsx
@@ -7,10 +7,15 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import search from '../../../../../assets/images/search.svg';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+
 
 const {
     LABEL_LOGO,
+    // eslint-disable-next-line
     LABEL_MAIN,
+    // eslint-disable-next-line
     LABEL_LIST_AUTHORS,
     LABEL_INPUT_SURNAME,
 } = HEADER_DICTIONARY;
@@ -19,7 +24,7 @@ const {
 
 const HeaderDesktop = (props) => {
     const [ value, setValue ] = useState('');
-
+    const { t } = useTranslation();
 
     return(
         <>
@@ -28,8 +33,8 @@ const HeaderDesktop = (props) => {
                     <div className={s.Container_beginBox}><Link to='/domain'>{LABEL_LOGO}</Link></div>
                     <div className={s.Container_centerBox}>
                         <ul className={s.List}>
-                            <li><Link to='/domain'>{LABEL_MAIN}</Link></li>
-                            <li><Link to='/domain/authors'>{LABEL_LIST_AUTHORS}</Link></li>
+                            <li><Link to='/domain'>{ t('main_page') }</Link></li>
+                            <li><Link to='/domain/authors'>{ t('author_list') }</Link></li>
                         </ul>
                     </div>
                     <div className={
@@ -39,8 +44,8 @@ const HeaderDesktop = (props) => {
                         )
                     }>
                         <ul className={s.LanguageSelector}>
-                            <li><Link>Русский</Link></li>
-                            <li><Link>Английский</Link></li>
+                            <li><Button onClick={() => i18next.changeLanguage('ru') }>Русский</Button></li>
+                            <li><Button onClick={() => i18next.changeLanguage('en') }>English</Button></li>
                         </ul>
                         <Form className="d-flex">
                             <Form.Control
